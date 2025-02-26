c5342b31837ef71343958622dcaa8f6801cbb46f
William Guillermin <william.guillermin@visioglobe.com>
2025-02-26 23:56:12 +0100

    feat: Added scroll on the onboarding screen

--- app/src/main/java/com/illiouchine/jm/ui/screen/OnBoardingScreen.kt
+++ app/src/main/java/com/illiouchine/jm/ui/screen/OnBoardingScreen.kt
@@ -1,25 +1,34 @@
 package com.illiouchine.jm.ui.screen
 
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import com.illiouchine.jm.R
 import com.illiouchine.jm.ui.composable.ViewPager
 import com.illiouchine.jm.ui.theme.JmTheme
@@ -44,47 +53,78 @@ fun OnBoardingScreen(
     )
 
     Scaffold(
-        modifier = Modifier.fillMaxSize().pointerInput(Unit){
-            detectHorizontalDragGestures { _, dragAmount ->
-
-//                offsetX = (offsetX + dragAmount).coerceIn(-300f, 0f)
-            }
-        },
-
+        modifier = Modifier.fillMaxSize(),
     ) { innerPadding ->
+
         var currentPageIndex by remember { mutableIntStateOf(0) }
-        Box(
+        var dragValue by remember { mutableFloatStateOf(0f) }
+
+        Column(
             modifier = modifier
                 .padding(innerPadding)
-                .fillMaxSize()
-                .padding(36.dp),
+                .padding(16.dp),
         ) {
             Text(
-                modifier = Modifier.align(Alignment.TopStart),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                fontSize = 8.em,
+                textAlign = TextAlign.Center,
+                lineHeight = 1.3.em,
                 text = stringResource(R.string.majority_judgment),
             )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .offset(x = -(dragValue/3).dp)
+                    .pointerInput(Unit) {
+                        detectDragGestures(
+                            onDragEnd = {
+                                when {
+                                    dragValue> 0 && currentPageIndex < (onBoardingPages.size - 1) -> currentPageIndex++
+                                    dragValue< 0 && currentPageIndex > 0 -> currentPageIndex--
 
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                text = onBoardingPages[currentPageIndex].text,
-            )
+                                }
+                                dragValue = 0f
+                            },
+                            onDrag = { change, dragAmount ->
+                                dragValue += -dragAmount.x
+                            }
+                        )
+                    },
+            ) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(horizontal = 24.dp)
+                        .fillMaxWidth(),
+                    text = onBoardingPages[currentPageIndex].text,
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
 
-            ViewPager(
-                modifier = Modifier.align(Alignment.BottomCenter),
-                pageSize = onBoardingPages.size,
-                currentPage = currentPageIndex,
-            )
+                Spacer(Modifier.weight(0.7f))
+                ViewPager(
+                    modifier = Modifier.weight(1f).wrapContentSize(),
+                    pageSize = onBoardingPages.size,
+                    currentPage = currentPageIndex,
+                )
 
-            if (currentPageIndex == onBoardingPages.size - 1) {
-                Button(
-                    modifier = Modifier.align(Alignment.BottomEnd),
-                    onClick = { onFinish() },
-                ) { Text(stringResource(R.string.button_finish)) }
-            } else {
-                Button(
-                    modifier = Modifier.align(Alignment.BottomEnd),
-                    onClick = { currentPageIndex++ },
-                ) { Text(stringResource(R.string.button_next)) }
+                if (currentPageIndex == onBoardingPages.size - 1) {
+                    TextButton(
+                        modifier = Modifier.weight(0.7f),
+                        onClick = { onFinish() },
+                    ) { Text(stringResource(R.string.button_finish)) }
+                } else {
+                    TextButton(
+                        modifier = Modifier.weight(0.7f),
+                        onClick = { currentPageIndex++ },
+                    ) { Text(stringResource(R.string.button_next)) }
+                }
             }
         }
     }
