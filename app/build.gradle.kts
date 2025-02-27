5b4e5d9180bc2f0053a1afd1f2dbd18b6b738078
domi41 <dominique.merle.41@gmail.com>
2025-02-27 18:31:36 +0100

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.illiouchine.jm"
         minSdk = 27
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
