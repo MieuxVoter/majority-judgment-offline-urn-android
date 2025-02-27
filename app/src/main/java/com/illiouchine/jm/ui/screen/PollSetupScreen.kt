5879080ca779dc05019f4b82e3307dffa0d0f8a3
domi41 <dominique.merle.41@gmail.com>
2025-02-27 18:29:18 +0100

    test: experiment with the test suite

--- app/src/main/java/com/illiouchine/jm/ui/screen/PollSetupScreen.kt
+++ app/src/main/java/com/illiouchine/jm/ui/screen/PollSetupScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,7 +64,7 @@ fun PollSetupScreen(
 ) {
 
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().testTag("screen_setup"),
         snackbarHost = {
             MjuSnackbar(
                 modifier = Modifier,
