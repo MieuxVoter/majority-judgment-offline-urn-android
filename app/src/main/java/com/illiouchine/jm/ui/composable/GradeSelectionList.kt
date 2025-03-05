93a17ea4a99303e98fb9267ed82a1737f2e11777
domi41 <dominique.merle.41@gmail.com>
2025-03-05 17:49:39 +0100

    chore(lint): imports

--- app/src/main/java/com/illiouchine/jm/ui/composable/GradeSelectionList.kt
+++ app/src/main/java/com/illiouchine/jm/ui/composable/GradeSelectionList.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.illiouchine.jm.R
 import com.illiouchine.jm.model.Grading
--- app/src/main/java/com/illiouchine/jm/ui/composable/JudgmentSummary.kt
+++ app/src/main/java/com/illiouchine/jm/ui/composable/JudgmentSummary.kt
@@ -1,6 +1,5 @@
 package com.illiouchine.jm.ui.composable
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
