9edf8ac97823eee45275d73b348d085da26c7378
domi41 <dominique.merle.41@gmail.com>
2025-03-06 22:21:33 +0100

    chore(lint)

--- app/src/main/java/com/illiouchine/jm/MainActivity.kt
+++ app/src/main/java/com/illiouchine/jm/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
