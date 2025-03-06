c76023529fcfd26907b441135e702d75ead13f33
domi41 <dominique.merle.41@gmail.com>
2025-03-06 22:56:10 +0100

    refacto: create component LinearMeritProfileCanvas

---
+++ app/src/main/java/com/illiouchine/jm/ui/composable/LinearMeritProfileCanvas.kt
@@ -0,0 +1,153 @@
+package com.illiouchine.jm.ui.composable
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
+import com.illiouchine.jm.model.Grading
+import fr.mieuxvoter.mj.ProposalResultInterface
+import fr.mieuxvoter.mj.TallyInterface
+import java.util.Locale
+import kotlin.math.round
+
+@Composable
+fun LinearMeritProfileCanvas(
+    modifier: Modifier = Modifier,
+    tally: TallyInterface,
+    proposalResult: ProposalResultInterface,
+    grading: Grading,
+) {
+
+    val textMeasurer = rememberTextMeasurer()
+    val contrastedColor = if (isSystemInDarkTheme()) Color.White else Color.Black
+    val waitAnimation = remember { Animatable(0f) } // :(|) oOok
+    val widthAnimation = remember { Animatable(0f) }
+    val outlineAnimation = remember { Animatable(0f) }
+    val percentageAnimation = remember { Animatable(0f) }
+
+    LaunchedEffect("apparition") {
+        // TODO: figure out how to NOT use the waitAnimation hack
+        waitAnimation.animateTo(1f)
+        waitAnimation.animateTo(0f)
+        waitAnimation.animateTo(1f)
+        waitAnimation.animateTo(0f)
+        widthAnimation.animateTo(1f, tween(1500))
+        outlineAnimation.animateTo(1f, tween(400))
+        waitAnimation.animateTo(1f)
+        waitAnimation.animateTo(0f)
+        percentageAnimation.animateTo(1f, tween(3000))
+    }
+
+    // Draw the linear merit profile of the proposal.
+    Canvas(
+        modifier = modifier,
+    ) {
+        val proposalTally = tally.proposalsTallies[proposalResult.index]
+        var offsetX = 0f
+        val medianGradeOutline = Path()
+        val balancedGradeWidth = size.width / grading.getAmountOfGrades()
+
+        for (gradeIndex in (0..<grading.getAmountOfGrades()).reversed()) {
+            var gradeWidth = ( // appalling indentation, please help
+                    (size.width * proposalTally.tally[gradeIndex].toFloat())
+                            /
+                            proposalTally.amountOfJudgments.toFloat()
+                    )
+            gradeWidth = lerp(balancedGradeWidth, gradeWidth, widthAnimation.value)
+            val gradeRectSize = Size(gradeWidth, size.height)
+            val gradeRectOffset = Offset(offsetX, 0f)
+
+            // Fill a rectangle with the color of the grade
+            drawRect(
+                color = grading.getGradeColor(gradeIndex),
+                size = gradeRectSize,
+                topLeft = gradeRectOffset,
+            )
+
+            // Show the percentage under each grade with at least one judgment
+            if (gradeWidth > 0f) {
+                val percentage = 100f * gradeWidth / size.width
+                val approximate = if (round(percentage) != percentage) {
+                    "~"
+                } else {
+                    ""
+                }
+                val measuredText =
+                    textMeasurer.measure(
+                        text = AnnotatedString(
+                            String.format(
+                                locale = Locale.FRANCE,
+                                format = "$approximate%.0f%%",
+                                percentage,
+                            )
+                        ),
+                        style = TextStyle(fontSize = 10.sp),
+                    )
+                drawText(
+                    textLayoutResult = measuredText,
+                    topLeft = gradeRectOffset + Offset(
+                        (gradeRectSize.width - measuredText.size.width) * 0.5f,
+                        size.height + 4.dp.toPx(),
+                    ),
+                    color = contrastedColor,
+                    alpha = 0.78f * percentageAnimation.value,
+                )
+            }
+
+            // Outline only the median grade
+            if (gradeIndex == proposalResult.analysis.medianGrade) {
+                medianGradeOutline.addRect(
+                    Rect(
+                        size = gradeRectSize,
+                        offset = gradeRectOffset,
+                    )
+                )
+            }
+
+            offsetX += gradeWidth
+        }
+
+        // Draw the median grade outline *after* drawing all the grade rectangles
+        drawPath(
+            color = contrastedColor,
+            path = medianGradeOutline,
+            style = Stroke(
+                width = 3.dp.toPx(),
+                join = StrokeJoin.Bevel,
+            ),
+            alpha = outlineAnimation.value,
+        )
+
+        // Vertical line in the middle, marking the median grade.
+        drawLine(
+            color = contrastedColor,
+            start = Offset(size.width * 0.5f, -3.dp.toPx()),
+            end = Offset(size.width * 0.5f, size.height + 3.dp.toPx()),
+            pathEffect = PathEffect.dashPathEffect(
+                intervals = floatArrayOf(3.dp.toPx(), 1.dp.toPx()),
+                phase = -1.dp.toPx(),
+            ),
+            strokeWidth = 1.dp.toPx(),
+            alpha = outlineAnimation.value,
+        )
+    }
+}
