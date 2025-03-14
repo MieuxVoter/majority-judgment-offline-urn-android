dc580584fe1d453e6c601e5f77b262b1175ed723
domi41 <dominique.merle.41@gmail.com>
2025-03-14 15:53:08 +0100

    test: vote, rescind, vote, confirm
    This is missing assertions about the amount of ballots in the urn.

--- app/src/main/java/com/illiouchine/jm/ui/screen/BallotSummaryScreen.kt
+++ app/src/main/java/com/illiouchine/jm/ui/screen/BallotSummaryScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,7 +38,8 @@ fun BallotSummaryScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(16.dp)
+            .testTag("ballot_summary"),
     ) {
 
         Text(
@@ -75,6 +77,8 @@ fun BallotSummaryScreen(
         ) {
 
             TextButton(
+                modifier = Modifier
+                    .testTag("ballot_summary_rescind"),
                 onClick = onCancel,
             ) {
                 Text(
@@ -86,6 +90,8 @@ fun BallotSummaryScreen(
             Spacer(Modifier.weight(1f))
 
             Button(
+                modifier = Modifier
+                    .testTag("ballot_summary_confirm"),
                 onClick = onConfirm,
             ) {
                 Text(
