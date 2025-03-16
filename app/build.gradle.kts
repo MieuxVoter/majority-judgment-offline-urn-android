d43e0c70a3ba088c261a716b68e25e1ec11fe498
domi41 <dominique.merle.41@gmail.com>
2025-03-16 17:45:02 +0100

    config(CI): configure detekt some more

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
     alias(libs.plugins.room)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.serialization)
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
 }
+
 
 android {
     namespace = "com.illiouchine.jm"
@@ -96,4 +98,61 @@ dependencies {
     androidTestImplementation(libs.cucumber.picocontainer)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+// Static code analyzer:
+//     ./gradlew detekt
+detekt {
+    // Version of detekt that will be used. When unspecified the latest detekt
+    // version found will be used. Override to stay on the same version.
+    toolVersion = "1.23.8"
+
+    // The directories where detekt looks for source files.
+    // Defaults to `files("src/main/java", "src/test/java", "src/main/kotlin", "src/test/kotlin")`.
+    //source.setFrom("src/main/java", "src/main/kotlin")
+
+    // Builds the AST in parallel. Rules are always executed in parallel.
+    // Can lead to speedups in larger projects. `false` by default.
+    parallel = false
+
+    // Define the detekt configuration(s) you want to use.
+    // Defaults to the default detekt configuration.
+    config.setFrom("../detekt.yml")
+
+    // Applies the config files on top of detekt's default config file. `false` by default.
+    buildUponDefaultConfig = false
+
+    // Turns on all the rules. `false` by default.
+    allRules = false
+
+    // Specifying a baseline file. All findings stored in this file in subsequent runs of detekt.
+    //baseline = file("path/to/baseline.xml")
+
+    // Disables all default detekt rulesets and will only run detekt with custom rules
+    // defined in plugins passed in with `detektPlugins` configuration. `false` by default.
+    disableDefaultRuleSets = false
+
+    // Adds debug output during task execution. `false` by default.
+    debug = false
+
+    // If set to `true` the build does not fail when there are any issues.
+    // Defaults to `false`.
+    ignoreFailures = true
+
+    // The build fails when there is at least one issue with this severity (or above).
+    // If set ot `Never`, the task will not fail regardless of the number of issues and their severities.
+    // If `ignoreFailures` is set to `true`, the value of this property is ignored.
+    // Defaults to `Error`
+    //failOnSeverity = io.gitlab.arturbosch.detekt.extensions.FailOnSeverity.Error
+
+    // Android: Don't create tasks for the specified build types (e.g. "release")
+    ignoredBuildTypes = listOf("release")
+
+    // Android: Don't create tasks for the specified build flavor (e.g. "production")
+    ignoredFlavors = listOf("production")
+
+    // Android: Don't create tasks for the specified build variants (e.g. "productionRelease")
+    ignoredVariants = listOf("productionRelease")
+
+    autoCorrect = true
 }
