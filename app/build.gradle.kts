06674256890a0f73e6d4984fafb86e0ecd6fbfe1
domi41 <dominique.merle.41@gmail.com>
2025-04-09 13:38:39 +0200

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.illiouchine.jm"
         minSdk = 27
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.1"
+        versionCode = 8
+        versionName = "1.3.0"
 
         // Ideally we'd have both, but support for multiple runners look experimental
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
