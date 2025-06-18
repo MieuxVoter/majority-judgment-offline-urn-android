56f58749bb3aabfd24bbf64ecdec608aa4cd8b6f
domi41 <dominique.merle.41@gmail.com>
2025-06-18 21:54:14 +0200

    fix: bunch of accessibility issues using TalkBack

--- app/src/main/java/com/illiouchine/jm/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/illiouchine/jm/ui/screen/HomeScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
