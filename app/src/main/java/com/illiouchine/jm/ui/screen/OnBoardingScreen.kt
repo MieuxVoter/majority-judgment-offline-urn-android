091038c269e151e8574b2da0d4ee6416961bff09
domi41 <dominique.merle.41@gmail.com>
2025-06-20 05:02:30 +0200

    fix(a11y): work a little on the onboarding

--- app/src/main/java/com/illiouchine/jm/ui/screen/OnBoardingScreen.kt
+++ app/src/main/java/com/illiouchine/jm/ui/screen/OnBoardingScreen.kt
@@ -44,7 +44,8 @@ import kotlin.math.absoluteValue
 
 
 private fun PagerState.isScrollOverLast(): Boolean =
-    (this.currentPage == this.pageCount -1 && this.currentPageOffsetFraction > 0)
+    (this.currentPage == this.pageCount - 1 && this.currentPageOffsetFraction > 0)
+
 
 @Composable
 fun OnBoardingScreen(
@@ -123,24 +124,23 @@ fun OnBoardingPage(
 ) {
     Box(
         modifier = modifier.graphicsLayer {
-            // Calculate the absolute offset for the current page from the
-            // scroll position. We use the absolute value which allows us to mirror
-            // any effects for both directions
+            // Calculate the absolute offset for the current page from the scroll position.
+            // We use the absolute value which allows us to mirror any effects for both directions.
             val pageOffset = pagerState.calculateCurrentOffsetForPage(pageIndex).absoluteValue
             alpha = lerp(
                 start = 0.1f,
                 stop = 1f,
-                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                fraction = 1f - pageOffset.coerceIn(0f, 1f),
             )
             scaleX = lerp(
                 start = 0.3f,
                 stop = 1f,
-                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                fraction = 1f - pageOffset.coerceIn(0f, 1f),
             )
             scaleY = lerp(
                 start = 0.3f,
                 stop = 1f,
-                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                fraction = 1f - pageOffset.coerceIn(0f, 1f),
             )
         },
     ) {
