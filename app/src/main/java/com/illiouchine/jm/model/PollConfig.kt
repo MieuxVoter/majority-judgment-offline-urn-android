f8bb9a7727956be6d5c4eac8118ffc5f0d29dec8
William Guillermin <guillermin.w@gmail.com>
2026-01-18 14:40:03 +0100

    Notify compose that pollconfig is stable

--- app/src/main/java/com/illiouchine/jm/model/PollConfig.kt
+++ app/src/main/java/com/illiouchine/jm/model/PollConfig.kt
@@ -1,8 +1,10 @@
 package com.illiouchine.jm.model
 
+import androidx.compose.runtime.Stable
 import com.illiouchine.jm.logic.DEFAULT_GRADING_QUALITY_VALUE
 import kotlinx.serialization.Serializable
 
+@Stable
 @Serializable
 data class PollConfig(
     val subject: String = "",
