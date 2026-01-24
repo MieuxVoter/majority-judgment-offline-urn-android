b5067431e322567274c5b12e2d75ef3a8b0baeb1
domi41 <dominique.merle.41@gmail.com>
2026-01-24 09:33:20 +0100

    fix: nav rail menu padding for Rtl layouts

--- app/src/main/java/com/illiouchine/jm/ui/scaffold/MjuScaffold.kt
+++ app/src/main/java/com/illiouchine/jm/ui/scaffold/MjuScaffold.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
@@ -24,6 +25,7 @@ import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.illiouchine.jm.ui.composable.MjuBottomBar
 import com.illiouchine.jm.ui.composable.MjuNavigationRail
@@ -52,6 +54,7 @@ fun MjuScaffold(
     // -----------------
     topBar: @Composable () -> Unit = {},
     // BottomBar property was removed ; use showMenu and menu properties instead
+    // IDEA: re-enable the property and yell/throw on use ?
     //bottomBar: @Composable () -> Unit = {},
     snackbarHost: @Composable () -> Unit = {},
     floatingActionButton: @Composable () -> Unit = {},
@@ -86,16 +89,27 @@ fun MjuScaffold(
         contentColor = contentColor,
         contentWindowInsets = contentWindowInsets,
     ) { originalPadding ->
-        val layoutDirection = LocalLayoutDirection.current
-        var actualPadding = originalPadding
         if (showMenu && isLandscape) {
+            // NOTE: the nav rail will always appear on the left, whatever the layout direction is.
+            // Therefore we need to compensate for this, as the PaddingValues API is start/end.
+            val layoutDirection = LocalLayoutDirection.current
+            val isLtr = (layoutDirection == LayoutDirection.Ltr)
             // WARN: We're using a magic number here (80) instead of measuring the navigation rail.
             // Since we create the nav rail at the very end I'm not even sure how we might do that.
             val railWidth = 80.dp + Theme.spacing.small
-            actualPadding = PaddingValues(
-                start = originalPadding.calculateStartPadding(layoutDirection) + railWidth,
+            // Let's update the padding to include our vertical navigation rail
+            val actualPadding = PaddingValues(
+                start = originalPadding.calculateStartPadding(layoutDirection) + if (isLtr) {
+                    railWidth
+                } else {
+                    0.dp
+                },
                 top = originalPadding.calculateTopPadding(),
-                end = originalPadding.calculateEndPadding(layoutDirection),
+                end = originalPadding.calculateEndPadding(layoutDirection) + if (isLtr) {
+                    0.dp
+                } else {
+                    railWidth
+                },
                 bottom = originalPadding.calculateBottomPadding(),
             )
             // We must process the content first, so that our navigation rail has a "higher zIndex".
@@ -109,7 +123,7 @@ fun MjuScaffold(
                 onItemSelected = onMenuItemSelected,
             )
         } else {
-            content(actualPadding)
+            content(originalPadding)
         }
     }
 }
@@ -145,10 +159,35 @@ private fun PreviewScaffoldLandscapeWithMenu() {
                 Text(
                     modifier = Modifier
                         .fillMaxWidth(),
-                    text = "I should NOT be behind the vertical menu on the left.",
+                    text = "I should NOT be behind the vertical menu on the left, and yet I should probably go to the right edge of the preview.",
                     fontStyle = FontStyle.Italic,
                 )
                 Spacer(Modifier.weight(1f))
+            }
+        }
+    }
+}
+
+@Preview(
+    showSystemUi = true,
+    uiMode = UI_MODE_NIGHT_YES,
+    device = "spec:width=511dp,height=891dp,orientation=landscape",
+)
+@Composable
+private fun PreviewScaffoldLandscapeWithMenuRtl() {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        JmTheme {
+            MjuScaffold(showMenu = true) {
+                Column(modifier = Modifier.padding(it)) {
+                    Spacer(Modifier.weight(1f))
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = "I should NOT be behind the vertical menu, I should be filled with joy and hot muffins, and the ever-nagging sense of doom that never fails to accompany my fellow contemporaries.",
+                        fontStyle = FontStyle.Italic,
+                    )
+                    Spacer(Modifier.weight(1f))
+                }
             }
         }
     }
