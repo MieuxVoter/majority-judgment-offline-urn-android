fc6a53780f5efcfad70051de6baf2045e782bb37
domi41 <dominique.merle.41@gmail.com>
2026-05-03 01:23:30 +0200

    feat: harden the preferential favoritism algorithm
    This is to make it anti-strategic, as MJ is.
    Right now I don't see any strategy that can thwart this algorithm, but I'm confident some clever fox might inb the future, so we might need to tweak this again in the future.

--- app/src/main/java/com/illiouchine/jm/service/OsmosisRepartitor.kt
+++ app/src/main/java/com/illiouchine/jm/service/OsmosisRepartitor.kt
@@ -3,71 +3,73 @@ package com.illiouchine.jm.service
 import com.illiouchine.jm.model.Poll
 
 /**
- * Computes a proportional representation of the proposals/candidates.
- * The returned list of proportions is normalized, that is its sum will always be 1.
- *
- * This code can be heavily optimized ; this is an initial draft.
- * The goal for now is to be as explicit and clear as possible.  Optimization will come later.
+ * Computes a proportional representation of the proposals (a.k.a. candidates).
+ * The returned list of proportions is normalized, meaning that its sum will always be 1.
  *
  * Qualities:
  * - backwards-compatible: identity with uninominal in case of extreme polarization
- * - robust: disincentivizes polarized voting (ie. "cheating"), just like Majority Judgment
+ * - robust: disincentivizes strategic voting, just like Majority Judgment
  * - holistic: uses all cast judgments, one way or another
  * - deterministic: no random
  * - exact: no approximation
- * - scalable: reasonably computable for billions of judges
+ * - scalable: reasonably computable for billions of judges and hundreds of candidates
  * - simple: elementary arithmetic
  *
  * Known Issues:
- * - not isomorphic with MJ ; could be mitigated using the DecreasingListConstrictor (berk)
- * - might be ever so slightly numerically unstable since we're handling IEE754 floats
+ * - not isomorphic with Majority Judgment
+ * - not extensively studied
+ *
+ * Notes:
+ * This code can be heavily optimized ; this is an initial draft.
+ * The goal for now is to be as explicit and clear as possible.  Optimization will come later.
  */
-class OsmosisRepartitor { // FavorityJudgmentRepartitor?  (still workshopping the name)
+class OsmosisRepartitor { // PreferentialFavoritismRepartitor?  (still workshopping the name)
 
     fun computeProportionalRepresentation(poll: Poll): List<Double> {
         val ballots = poll.ballots
         val amountOfProposals = poll.pollConfig.proposals.size
 
         if (ballots.isEmpty()) {
-            return List(
-                size = amountOfProposals,
-                init = { 0.0 },
-            )
+            return List(size = amountOfProposals) { 0.0 }
         }
 
         val amountOfBallots = ballots.size
         val acceptationGradeThreshold = poll.pollConfig.grading.acceptationThreshold
 
-        // Step 1: For each proposal, count the acceptation judgments they received
-        //         that were the highest judgments of their respective ballots.
-        val favoritism: List<Int> = List(
-            size = amountOfProposals,
-            init = { proposalIndex ->
-                ballots.fold(0) { points, ballot ->
-                    val proposalGrade = ballot.gradeOf(proposalIndex)
-                    val highestGradeGiven = ballot.getHighestGrade()
-                    val isBallotRejection = highestGradeGiven < acceptationGradeThreshold
-                    points + if (!isBallotRejection && proposalGrade == highestGradeGiven) {
-                        1
-                    } else {
-                        0
-                    }
+        // Step 1: Establish an initial proportional representation using only the favorites,
+        //         the proposals that received the highest acceptation grades of each ballot.
+        //         This is done in the simple fashion of uninominal voting.
+        val favoritism: List<Double> = List(size = amountOfProposals) { proposalIndex ->
+            ballots.sumOf { ballot ->
+                val proposalGrade = ballot.gradeOf(proposalIndex)
+                val highestGradeGiven = ballot.getHighestGrade()
+                val isBallotRejection = highestGradeGiven < acceptationGradeThreshold
+                if (isBallotRejection) {
+                    // Rule: Proposals that received rejection grades are not considered favorites,
+                    //       even if they received the highest grade of the ballot.
+                    0.0
+                } else if (proposalGrade != highestGradeGiven) {
+                    // Rule: We only care about the favorite proposals in this initial step.
+                    //       It ensures backwards compatibility and resilience to polarized voting.
+                    0.0
+                } else {
+                    // Rule: Multiple favorites of a ballot receive each a fraction of a vote.
+                    //       This fosters resilience to strategic voting.
+                    val amountOfFavorites = ballot.countProposalsWithGrade(proposalGrade)
+                    1.0 / amountOfFavorites.toDouble()
                 }
-            },
-        )
+            }
+        }
 
-        // Step 2: Establish an initial proportional representation using only the favoritism data.
-        val totalFavoritism = favoritism.fold(0) { total, value -> total + value }
-        val initialProportions: List<Double> = List(
-            size = amountOfProposals,
-            init = {
-                favoritism[it].toDouble() / totalFavoritism.toDouble()
-            },
-        )
+        val totalFavoritism = favoritism.sum()
+        val initialProportions: List<Double> = List(size = amountOfProposals) { proposalIndex ->
+            favoritism[proposalIndex] / totalFavoritism
+        }
 
+        // Step 2: For each pair of proposals, use the preference for one over the other in each
+        //         ballot to transfer a fraction of the initial proportion from one proposal to
+        //         another, if neither proposal was a favorite in that ballot.
         val finalProportions: MutableList<Double> = initialProportions.toMutableList()
-
-        // Step 3: For each pair of proposals, seep representation by osmosis.
         val seepingNormalization = amountOfBallots * (amountOfProposals - 1)
         for (proposalIndexA in (0..<amountOfProposals - 1)) {
             for (proposalIndexB in (proposalIndexA + 1..<amountOfProposals)) {
@@ -108,7 +110,9 @@ class OsmosisRepartitor { // FavorityJudgmentRepartitor?  (still workshopping th
             }
         }
 
-        // Step 4: Done.
+        // Step 3: ???
+
+        // Step 4: Profit!
         return finalProportions
     }
 }
