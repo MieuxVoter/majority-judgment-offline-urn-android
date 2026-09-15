8b38d47d00efdecf9b955439b4089808e22adc6a
domi41 <dominique.merle.41@gmail.com>
2026-09-15 21:19:27 +0200

    config: mark some more models as Stable

--- app/src/main/java/com/illiouchine/jm/model/ProposalTally.kt
+++ app/src/main/java/com/illiouchine/jm/model/ProposalTally.kt
@@ -1,10 +1,12 @@
 package com.illiouchine.jm.model
 
+import androidx.compose.runtime.Stable
 import fr.mieuxvoter.mj.ProposalTallyInterface
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toPersistentList
 import java.math.BigInteger
 
+@Stable
 data class ProposalTally(
     val tally: ImmutableList<BigInteger>,
     val amountOfJudgments: BigInteger,
@@ -13,6 +15,6 @@ data class ProposalTally(
 fun ProposalTallyInterface.toProposalTally(): ProposalTally {
     return ProposalTally(
         tally = this.tally.toPersistentList(),
-        amountOfJudgments = this.amountOfJudgments
+        amountOfJudgments = this.amountOfJudgments,
     )
 }
