da4f30de15ec2aff7cb8bcd7a839830a61190147
domi41 <dominique.merle.41@gmail.com>
2026-09-15 18:12:41 +0200

    chore: add a Compose Preview for the Opinion Profile Bar Chart

--- app/src/main/java/com/illiouchine/jm/ui/composable/plot/OpinionProfileBarChart.kt
+++ app/src/main/java/com/illiouchine/jm/ui/composable/plot/OpinionProfileBarChart.kt
@@ -1,7 +1,10 @@
 package com.illiouchine.jm.ui.composable.plot
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -14,16 +17,23 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.illiouchine.jm.R
 import com.illiouchine.jm.extensions.reversedIf
 import com.illiouchine.jm.extensions.smartFormat
+import com.illiouchine.jm.model.Ballot
+import com.illiouchine.jm.model.Judgment
 import com.illiouchine.jm.model.Poll
 import com.illiouchine.jm.model.Tally
+import com.illiouchine.jm.model.toTally
 import com.illiouchine.jm.ui.composable.plot.component.PlotTitle
 import com.illiouchine.jm.ui.composable.plot.utils.favorIntLineCountForBars
+import com.illiouchine.jm.ui.preview.PreviewDataFaker
+import com.illiouchine.jm.ui.theme.JmTheme
 import com.illiouchine.jm.ui.theme.Theme
+import fr.mieuxvoter.mj.CollectedTally
 import ir.ehsannarmani.compose_charts.ColumnChart
 import ir.ehsannarmani.compose_charts.models.BarProperties
 import ir.ehsannarmani.compose_charts.models.Bars
@@ -35,13 +45,11 @@ import ir.ehsannarmani.compose_charts.models.LabelHelperProperties
 import ir.ehsannarmani.compose_charts.models.LabelProperties
 
 /**
- * Idea: This should take the form of a merit profile, as it IS one, for the poll itself.
+ * An Opinion Profile shows how many judgments of each grade were cast in the poll,
+ * across all candidates.
  *
- * An opinion profile shows how many judgments of each grade were cast in the poll,
- * "tous candidats confondus".
- * This helps getting a sense of the overall feel of the judges for the whole set of candidates.
- * This is especially useful to poll administrators in check since they chose the set of candidates.
- * It could be used constitutionally to throw out the whole poll and start it anew with new administrators.
+ * This helps to get a sense of the overall feel of the voters for the whole set of candidates.
+ * This is especially useful to poll administrators since they chose the set of candidates.
  *
  * Ideally the bars in this plot should be made of strata, as many as there are proposals.
  * The lib does not seem to allow this, so we might have to do it ourselves.
@@ -94,71 +102,140 @@ fun OpinionProfileBarChart(
 
     val horizontalLinesCount = favorIntLineCountForBars(barData)
 
-    ColumnChart(
-        modifier = modifier
-            // We need to fix the compose-chart lib upstream in order to show this.
-            // We do want to iterate over the labels, but we need to say the values too.
-            // We work around this by computing a textual data description (see PlotTitle below)
-            .clearAndSetSemantics {},
-        data = barData,
-        barProperties = BarProperties(
-            thickness = 32.dp,
-            spacing = 0.dp,
-            cornerRadius = Bars.Data.Radius.Rectangle(
-                topLeft = 4.dp,
-                topRight = 4.dp,
+    Column {
+        ColumnChart(
+            modifier = modifier
+                // We need to fix the compose-chart lib upstream in order to show this.
+                // We do want to iterate over the labels, but we need to say the values too.
+                // We work around this by computing a textual data description (see PlotTitle below)
+                .clearAndSetSemantics {},
+            data = barData,
+            barProperties = BarProperties(
+                thickness = 32.dp,
+                spacing = 0.dp,
+                cornerRadius = Bars.Data.Radius.Rectangle(
+                    topLeft = 4.dp,
+                    topRight = 4.dp,
+                ),
             ),
-        ),
-        labelProperties = LabelProperties(
-            enabled = true,
-            textStyle = TextStyle.Default.copy(
-                fontSize = 10.sp,
-                textAlign = TextAlign.End,
-                color = Theme.colorScheme.onBackground,
-            ),
-        ),
-        indicatorProperties = HorizontalIndicatorProperties(
-            textStyle = TextStyle.Default.copy(
-                fontSize = 12.sp,
-                textAlign = TextAlign.End,
-                color = Theme.colorScheme.onBackground,
-            ),
-            contentBuilder = {
-                it.smartFormat()
-            },
-            count = IndicatorCount.CountBased(horizontalLinesCount),
-        ),
-        dividerProperties = DividerProperties(
-            enabled = false,
-        ),
-        gridProperties = GridProperties(
-            enabled = true,
-            xAxisProperties = GridProperties.AxisProperties(
+            labelProperties = LabelProperties(
                 enabled = true,
-                lineCount = horizontalLinesCount,
+                textStyle = TextStyle.Default.copy(
+                    fontSize = 10.sp,
+                    textAlign = TextAlign.End,
+                    color = Theme.colorScheme.onBackground,
+                ),
             ),
-            yAxisProperties = GridProperties.AxisProperties(
+            indicatorProperties = HorizontalIndicatorProperties(
+                textStyle = TextStyle.Default.copy(
+                    fontSize = 12.sp,
+                    textAlign = TextAlign.End,
+                    color = Theme.colorScheme.onBackground,
+                ),
+                contentBuilder = {
+                    it.smartFormat()
+                },
+                count = IndicatorCount.CountBased(horizontalLinesCount),
+            ),
+            dividerProperties = DividerProperties(
                 enabled = false,
             ),
+            gridProperties = GridProperties(
+                enabled = true,
+                xAxisProperties = GridProperties.AxisProperties(
+                    enabled = true,
+                    lineCount = horizontalLinesCount,
+                ),
+                yAxisProperties = GridProperties.AxisProperties(
+                    enabled = false,
+                ),
+            ),
+            labelHelperProperties = LabelHelperProperties(
+                enabled = false,
+            ),
+        )
+
+        // Hotfix for bottom padding being too small when x-axis labels are rotated.
+        // This must stay a magic value, since it's a hotfix hack and not theme related.
+        Spacer(modifier = Modifier.padding(vertical = 26.dp))
+
+        val plotTitle = stringResource(R.string.plot_title_opinion_profile)
+        PlotTitle(
+            modifier = Modifier.semantics {
+                contentDescription = buildString {
+                    append(plotTitle)
+                    append("\n")
+                    append(dataDescription)
+                }
+            },
+            text = plotTitle,
+        )
+    }
+}
+
+@Preview(
+    name = "Phone (Portrait)",
+    showSystemUi = false,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    fontScale = 1.0f,
+)
+@Composable
+fun OpinionProfileBarChartPreview() {
+    val poll = Poll(
+        id = 1,
+        pollConfig = PreviewDataFaker.pollConfig(
+            amountOfProposals = 3,
         ),
-        labelHelperProperties = LabelHelperProperties(
-            enabled = false,
+        ballots = listOf(
+            Ballot(
+                judgments = listOf(
+                    Judgment(proposal = 0, grade = 0),
+                    Judgment(proposal = 1, grade = 1),
+                    Judgment(proposal = 2, grade = 2),
+                ),
+            ),
+            Ballot(
+                judgments = listOf(
+                    Judgment(proposal = 0, grade = 1),
+                    Judgment(proposal = 1, grade = 2),
+                    Judgment(proposal = 2, grade = 3),
+                ),
+            ),
+            Ballot(
+                judgments = listOf(
+                    Judgment(proposal = 0, grade = 2),
+                    Judgment(proposal = 1, grade = 3),
+                    Judgment(proposal = 2, grade = 4),
+                ),
+            ),
+            Ballot(
+                judgments = listOf(
+                    Judgment(proposal = 0, grade = 0),
+                    Judgment(proposal = 1, grade = 0),
+                    Judgment(proposal = 2, grade = 0),
+                ),
+            ),
         ),
     )
 
-    // Hotfix for bottom padding being too small when x-axis labels are rotated.
-    // This must stay a magic value, since it's a hotfix hack and not theme related.
-    Spacer(modifier = Modifier.padding(vertical = 26.dp))
+    // Refactor the following into a service (but first recode the MJ lib in Kotlin)
+    val amountOfProposals = poll.pollConfig.proposals.size
+    val amountOfGrades = poll.pollConfig.grading.getAmountOfGrades()
+    val tally = CollectedTally(amountOfProposals, amountOfGrades)
 
-    val plotTitle = stringResource(R.string.plot_title_opinion_profile)
-    PlotTitle(
-        modifier = Modifier.semantics {
-            contentDescription = buildString {
-                append(plotTitle)
-                append("\n")
-                append(dataDescription)
-            }
-        },
-        text = plotTitle,
-    )
+    poll.pollConfig.proposals.forEachIndexed { proposalIndex, _ ->
+        val voteResult = poll.judgments.filter { it.proposal == proposalIndex }
+        voteResult.forEach { judgment ->
+            tally.collect(proposalIndex, judgment.grade)
+        }
+    }
+    // ----------------------------------------------------------------------------
+
+    JmTheme {
+        OpinionProfileBarChart(
+            modifier = Modifier.height(400.dp).padding(8.dp),
+            poll = poll,
+            tally = tally.toTally(),
+        )
+    }
 }
