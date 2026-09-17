a42dff04327a3d284ffae9b9fb5ecb3167d0a534
domi41 <dominique.merle.41@gmail.com>
2026-09-17 12:45:44 +0200

    feat: add background patterns to the opinion profile

--- app/src/main/java/com/illiouchine/jm/ui/composable/plot/OpinionProfileBarChartKoala.kt
+++ app/src/main/java/com/illiouchine/jm/ui/composable/plot/OpinionProfileBarChartKoala.kt
@@ -1,16 +1,30 @@
 package com.illiouchine.jm.ui.composable.plot
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
@@ -26,15 +40,18 @@ import com.illiouchine.jm.model.Poll
 import com.illiouchine.jm.model.Tally
 import com.illiouchine.jm.model.toTally
 import com.illiouchine.jm.ui.composable.plot.component.PlotTitle
+import com.illiouchine.jm.ui.composable.plot.component.getPatternBrushes
 import com.illiouchine.jm.ui.composable.spacer.MediumVerticalSpacer
 import com.illiouchine.jm.ui.preview.PreviewDataFaker
 import com.illiouchine.jm.ui.theme.JmTheme
 import com.illiouchine.jm.ui.theme.Theme
 import fr.mieuxvoter.mj.CollectedTally
-import io.github.koalaplot.core.bar.DefaultBar
+import io.github.koalaplot.core.animation.StartAnimationUseCase
+import io.github.koalaplot.core.animation.StartAnimationUseCase.ExecutionType
 import io.github.koalaplot.core.bar.DefaultBarPosition
 import io.github.koalaplot.core.bar.DefaultVerticalBarPlotEntry
 import io.github.koalaplot.core.bar.VerticalBarPlot
+import io.github.koalaplot.core.style.KoalaPlotTheme
 import io.github.koalaplot.core.xygraph.AxisContent
 import io.github.koalaplot.core.xygraph.CategoryAxisModel
 import io.github.koalaplot.core.xygraph.CategoryAxisOffset
@@ -45,7 +62,7 @@ import io.github.koalaplot.core.xygraph.rememberGridStyle
 
 
 @Composable
-fun AxisLabel(
+private fun AxisLabel(
     label: String,
     modifier: Modifier = Modifier,
 ) {
@@ -56,6 +73,42 @@ fun AxisLabel(
         modifier = modifier,
         overflow = TextOverflow.Ellipsis,
         maxLines = 1,
+    )
+}
+
+
+@Composable
+private fun PatternBar(
+    brush: Brush,
+    patternBrush: Brush,
+    color: Color,
+    modifier: Modifier = Modifier,
+    shape: Shape = RectangleShape,
+    border: BorderStroke? = null,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .then(if (border != null) Modifier.border(border, shape) else Modifier)
+            .background(brush = brush, shape = shape)
+            .clip(shape),
+    )
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .drawWithContent {
+                drawRect(
+                    brush = patternBrush,
+                    colorFilter = ColorFilter.tint(
+                        color = lerp(
+                            start = color,
+                            stop = Color.Black,
+                            fraction = 0.2f,
+                        ),
+                    ),
+                )
+            }
+            .clip(shape),
     )
 }
 
@@ -73,17 +126,22 @@ fun OpinionProfileBarChartKoala(
     highestGradeToLowestGrade: Boolean = false,
     animated: Boolean = true,
 ) {
-    val opinionTally = List(grading.grades.size) { gradeIndex ->
-        tally.proposalsTallies.sumOf { proposalTally ->
-            proposalTally.tally[gradeIndex]
+    val gradesNames = grading.grades.map { stringResource(it.name) }
+
+    val opinionTally = remember(
+        key1 = tally,
+        key2 = highestGradeToLowestGrade,
+    ) {
+        List(grading.grades.size) { gradeIndex ->
+            tally.proposalsTallies.sumOf { proposalTally ->
+                proposalTally.tally[gradeIndex]
+            }
         }
     }
 
-    val gradesNames = grading.grades.map { stringResource(it.name) }
-
     val dataDescription = remember(
-        tally,
-        highestGradeToLowestGrade,
+        key1 = tally,
+        key2 = highestGradeToLowestGrade,
     ) {
         buildString {
             grading.grades
@@ -100,20 +158,25 @@ fun OpinionProfileBarChartKoala(
         }
     }
 
-    val barData = List(grading.grades.size) { gradeIndex ->
-        DefaultVerticalBarPlotEntry(
-            x = gradesNames[gradeIndex],
-            y = DefaultBarPosition(
-                start = 0L,
-                end = opinionTally[gradeIndex].toLong(),
-            ),
-        )
-    }.reversedIf(highestGradeToLowestGrade)
+    val barData = remember(
+        key1 = tally,
+        key2 = highestGradeToLowestGrade,
+    ) {
+        List(grading.grades.size) { gradeIndex ->
+            DefaultVerticalBarPlotEntry(
+                x = gradesNames[gradeIndex],
+                y = DefaultBarPosition(
+                    start = 0L,
+                    end = opinionTally[gradeIndex].toLong(),
+                ),
+            )
+        }.reversedIf(highestGradeToLowestGrade)
+    }
 
     val maxValue = opinionTally.max()
+    val brushes = getPatternBrushes()
 
     Column {
-
         XYGraph(
             modifier = modifier,
             xAxisModel = CategoryAxisModel(
@@ -127,7 +190,7 @@ fun OpinionProfileBarChartKoala(
                 labels = {
                     AxisLabel(
                         label = it,
-                        modifier = Modifier.padding(top = 2.dp),
+                        modifier = Modifier.padding(top = 12.dp),
                     )
                 },
                 title = {},
@@ -154,11 +217,27 @@ fun OpinionProfileBarChartKoala(
                     } else {
                         barIndex
                     }
-                    DefaultBar(
+                    PatternBar(
                         brush = SolidColor(grading.getGradeColor(gradeIndex)),
+                        patternBrush = brushes[gradeIndex],
+                        color = grading.getGradeColor(gradeIndex),
                         modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(
+                            topStart = 8f,
+                            topEnd = 8f,
+                            bottomStart = 0f,
+                            bottomEnd = 0f,
+                        )
                     )
                 },
+                startAnimationUseCase = StartAnimationUseCase(
+                    executionType = if (animated) {
+                        ExecutionType.Default
+                    } else {
+                        ExecutionType.None
+                    },
+                    KoalaPlotTheme.animationSpec,
+                )
             )
         }
 
@@ -237,7 +316,9 @@ fun OpinionProfileBarChartKoalaPreview() {
     JmTheme {
         Column {
             OpinionProfileBarChartKoala(
-                modifier = Modifier.height(300.dp).padding(8.dp),
+                modifier = Modifier
+                    .height(300.dp)
+                    .padding(8.dp),
                 tally = tally.toTally(),
                 grading = Grading.Quality5Grading,
                 animated = false,
@@ -245,11 +326,13 @@ fun OpinionProfileBarChartKoalaPreview() {
             MediumVerticalSpacer()
             MediumVerticalSpacer()
             OpinionProfileBarChartKoala(
-                modifier = Modifier.height(300.dp).padding(8.dp),
+                modifier = Modifier
+                    .height(300.dp)
+                    .padding(8.dp),
                 tally = tally.toTally().copy(),
                 grading = Grading.Quality5Grading,
                 highestGradeToLowestGrade = true,
-                animated = false,
+                animated = true,
             )
         }
     }
